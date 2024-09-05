5b41f504e12f67afd68351bef16c23374ebfdfb9
felipe-fatec <felipe.ferrare@fatec.sp.gov.br>
2024-09-05 16:21:19 -0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,6 +1,13 @@
+'''
+    Modulo de testes usando o Flask no Github
+'''
+
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+    '''
+        Essa função é a função basica a ser chamada quando o caminho raiz for invocado
+    '''
     return 'Hello, World!'
